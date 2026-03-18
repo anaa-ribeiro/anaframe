a231e7f4476bb37724a8a14416bd572fabf3c388
anaa-ribeiro <ana.ribeiro11@gmail.com>
2026-03-18 12:20:41 -0400

    aula6

---
+++ backend/src/todos/dto/use-case/create-todo.use-case.ts
@@ -0,0 +1,24 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { CreateTodoRepository } from "../repository";
+import { CreateTodoDto } from "../create-todo.dto";
+@Injectable()
+export class CreateTodoUseCase {
+    constructor(
+        private readonly createTodoRepository: CreateTodoRepository,
+        private readonly logger: Logger, 
+    ) {}
+        
+async execute (data: CreateTodoDto) {
+    try {
+        this.logger.log('Creating toDo...');
+        const todo = await this.createTodoRepository.execute(data);
+        this.logger.log('ToDo created successFully');
+        return todo;
+     }  catch (error) {
+        this.logger.error(error);
+        throw new Error('Failed to create toDo');
+     } 
+
+       
+    }
+}
