6996d68ceed32e6c347c702f93aedf7a7197fa57
anaa-ribeiro <ana.ribeiro11@gmail.com>
2026-04-08 11:30:55 -0400

    finalmente funcionou

--- backend/src/todos/repository/create-todo.repository.ts
+++ backend/src/todos/repository/create-todo.repository.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient, Todo } from 'generated/prisma/client';
 import { PrismaService } from 'src/shared/databases/prisma.databases';
 import { CreateTodoDto } from '../dto/create-todo.dto';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class CreateTodoRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  async execute(data: CreateTodoDto): Promise<Todo> {
+  async execute(data: CreateTodoDto) {
     const prisma = this.prisma as PrismaClient;
     return prisma.todo.create({ data });
   }
