4b1ef80cc8fed5971f12b5ce674aaadefaedad6c
anaa-ribeiro <ana.ribeiro11@gmail.com>
2026-05-13 12:15:02 -0400

    contato3

--- backend/src/auth/use-cases/register.use-cases.ts
+++ backend/src/auth/use-cases/register.use-cases.ts
@@ -1,1 +1,40 @@
-//oi
+import { BadRequestException, Injectable, Logger } from "@nestjs/common";
+import { CreateUserRepository, FindUseByEmailRepository } from "../repository";
+import { JwtService } from "@nestjs/jwt";
+import { RegisterDTO } from "../dto/register.dto";
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class RegisterUseCases {
+    constructor(
+     private readonly findUserByEmailRepository: FindUseByEmailRepository,
+     private readonly createUserRepository: CreateUserRepository,
+     private readonly jwtService: JwtService,
+     private readonly logger: Logger,
+    ) {} 
+
+    async execute(data: RegisterDTO) {
+        this.logger.log('Registering user...');
+
+        const existingUser = await this.findUserByEmailRepository.findByEmail(
+            data.email,
+        );
+        if (existingUser) {
+            throw new BadRequestException('Email exists');
+        }
+        const paswodHash = await bcrypt.hash(data.password, 10);
+        
+        const user = await this.createUserRepository.create({
+            name:data.name,
+            email:data.email,
+            paswodHash,
+        });
+
+        const payload = {sub: user.id, email: user.email};
+        const acessToken = this.jwtService.sign(payload);
+
+        this.logger.log('User registered successfully!');
+
+        return { acessToken, user };
+    }
+}
