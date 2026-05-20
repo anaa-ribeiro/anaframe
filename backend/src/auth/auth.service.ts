6f6b85fe3fea4ac21c455e1752af49935ea900d7
anaa-ribeiro <ana.ribeiro11@gmail.com>
2026-05-20 12:18:12 -0400

    contato5

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,1 +1,22 @@
-//oi
+import { Injectable } from "@nestjs/common";
+import { RegisterUseCases } from "./use-cases/register.use-cases";
+import { LoginUseCases } from "./use-cases/login.use-cases";
+import { RegisterDTO } from "./dto/register.dto";
+import { LoginDTO } from "./dto/login.dto";
+
+@Injectable()
+    export class AuthService{
+        constructor( 
+            private readonly registerUseCases: RegisterUseCases,
+            private readonly loginUseCases: LoginUseCases,
+        ){}
+
+        async register(data: RegisterDTO) {
+            return await this.registerUseCases.execute(data);
+        }
+        async login(data:LoginDTO ) {
+            return await this.loginUseCases.execute(data);
+        }
+        
+    }
+  
